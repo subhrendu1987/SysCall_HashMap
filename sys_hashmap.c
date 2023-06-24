3f711628419f960d342d93b47ab3cdb6ee786ff5
subhrendu1987 <subhrendu@idrbt.ac.in>
2023-06-25 00:39:24 +0530

    modified:   sys_hashmap.c

--- sys_hashmap.c
+++ sys_hashmap.c
@@ -1,21 +1,45 @@
 #include <linux/syscalls.h>
 #include <linux/hashmap.h>
 #include <linux/slab.h>
+#include <linux/proc_fs.h>
+
+#define PROC_FILENAME "hashmap"
+
+struct hashmap *map;
+
+static int hashmap_proc_show(struct seq_file *m, void *v)
+{
+    // Iterate over the hashmap and print its contents to the /proc file
+    // You can modify this function to display the hashmap in the desired format
+
+    return 0;
+}
+
+static int hashmap_proc_open(struct inode *inode, struct file *file)
+{
+    return single_open(file, hashmap_proc_show, NULL);
+}
+
+static const struct file_operations hashmap_proc_fops = {
+    .owner = THIS_MODULE,
+    .open = hashmap_proc_open,
+    .read = seq_read,
+    .llseek = seq_lseek,
+    .release = single_release,
+};
 
 asmlinkage long sys_<syscall_name>(unsigned int size)
 {
-    struct hashmap *map;
-
     // Create the hashmap
     map = hashmap_new(size, GFP_KERNEL);
     if (!map)
         return -ENOMEM;
 
+    // Register the /proc file to expose the hashmap
+    proc_create(PROC_FILENAME, 0, NULL, &hashmap_proc_fops);
+
     // Perform operations on the hashmap
     // For example, you can insert, update, or delete entries
-
-    // Clean up the hashmap
-    hashmap_free(map);
 
     return 0;
 }
